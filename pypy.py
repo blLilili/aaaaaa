5dc0c2bfc8baa39843e5a2721f8d667ac51dab4f
blLilili <biboklilivbj@]mail.com>
2024-09-30 15:03:51 +0200

    hit me and tell me ur min mwah

--- pypy.py
+++ pypy.py
@@ -1,1 +1,16 @@
-print(":3")
+import random
+
+def item10():
+    number = []
+    for i in range(10):
+        number.append(random.randint(1, 100))
+    return number
+
+def decide(listyy :list, value :int):
+    return value in listyy
+
+
+def main():
+    
+    current_list = list()
+    message = "done." if decide(current_list, 24) else "Nince."
